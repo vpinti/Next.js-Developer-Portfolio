f987636fbdfc8414a5f0e25982664ba643976ab6
Vittorio Pinti <vittorio.pinti@gmail.it>
2023-04-18 23:00:53 +0200

    sezione about me responsive

--- src/pages/about.js
+++ src/pages/about.js
@@ -41,9 +41,9 @@ const about = () => {
     </Head>
     <main className='flex w-full flex-col items-center justify-center dark:text-light'>
         <Layout className='pt-16'>
-            <AnimatedText text='Passion Fuels Purpose!' className='mb-16' />
-            <div className='grid w-full grid-cols-8 gap-16'>
-                <div className='col-span-3 flex flex-col items-start justify-start'>
+            <AnimatedText text='Passion Fuels Purpose!' className='mb-16 lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8' />
+            <div className='grid w-full grid-cols-8 gap-16 sm:gap-8'>
+                <div className='col-span-3 xl:col-span-4 flex flex-col items-start justify-start md:order-2 md:col-span-8'>
                     <h2 className='mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75'>Biography</h2>
                     <p className='font-medium'>
                         Hi, I&apos;m Vittorio Pinti, a web developer and UI/UX designer with a passion for creating beautiful, functional, 
@@ -60,33 +60,33 @@ const about = () => {
                         every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.
                     </p>
                 </div>
-                <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light'>
+                <div className='col-span-3 xl:col-span-4 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light md:order-1 md:col-span-8'>
                     <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light'/>
                     <Image src={profilePic} alt='Vittorio Pinti' className='w-full h-auto rounded-2xl' 
                     priority
                     sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw' 
                     />
                 </div>
-                <div className='col-span-2 flex flex-col items-end justify-between'>
-                    <div className='flex flex-col items-end justify-center'>
-                        <span className='inline-block text-7xl font-bold'>
+                <div className='col-span-2 xl:col-span-8 xl:flex-row flex flex-col items-end justify-between xl:items-center md:order-3'>
+                    <div className='flex flex-col items-end justify-center xl:items-center'>
+                        <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5l xs:text-4xl'>
                             <AnimatedNumbers value={50} />+
                         </span>
-                        <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75'>satisfied clients</h2>
+                        <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm'>satisfied clients</h2>
                     </div>
 
-                    <div className='flex flex-col items-end justify-center'>
-                        <span className='inline-block text-7xl font-bold'>
+                    <div className='flex flex-col items-end justify-center xl:items-center'>
+                        <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5l xs:text-4xl'>
                         <AnimatedNumbers value={40} />+
                         </span>
-                        <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75'>projects completed</h2>
+                        <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm'>projects completed</h2>
                     </div>
 
-                    <div className='flex flex-col items-end justify-center'>
-                        <span className='inline-block text-7xl font-bold'>
+                    <div className='flex flex-col items-end justify-center xl:items-center'>
+                        <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5l xs:text-4xl'>
                         <AnimatedNumbers value={5} />+
                         </span>
-                        <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75'>years of experience</h2>
+                        <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm'>years of experience</h2>
                     </div>                
 
                 </div>
